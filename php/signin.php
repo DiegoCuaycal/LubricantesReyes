64cd5e07d2412687afa997fe03f86e931fa1ce3d
Michael574a <139634342+Michael574a@users.noreply.github.com>
2023-07-23 19:58:58 -0500

    cambio signin
    cambio signin

--- php/signin.php
+++ php/signin.php
@@ -3,48 +3,53 @@
 $mensaje = "";
 $error = false;
 
+$nombre = $_POST['nombre'];
+$contraseña = $_POST['contraseña'];
+$cedula = $_POST['cedula'];
+$apellido = $_POST['apellido'];
+$email = $_POST['email'];
 
-    $nombre = $_POST['nombre'];
-    $contraseña = $_POST['contraseña'];
-    $cedula = $_POST['cedula'];
-    $apellido = $_POST['apellido'];
-    $email = $_POST['email'];
 
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $error = true;
+    $mensaje = "Correo electrónico inválido";
+} elseif (strlen($contraseña) < 5) {
+    $error = true;
+    $mensaje = "La contraseña debe tener al menos 8 caracteres";
+} else {
     
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $conn = new mysqli('localhost', 'root', 'root', 'lubricante', '3306');
+    if ($conn->connect_error) {
         $error = true;
-        $mensaje = "Correo electrónico inválido";
-    } elseif (strlen($contraseña) < 8) {
-        $error = true;
-        $mensaje = "La contraseña debe tener al menos 8 caracteres";
+        $mensaje = "Error de conexión a la base de datos: " . $conn->connect_error;
     } else {
-        $conn = new mysqli('localhost', 'root', 'root', 'lubricante', '3306');
-        if ($conn->connect_error) {
-            $error = true;
-            $mensaje = "Error de conexión a la base de datos: " . $conn->connect_error;
+        
+        $stmt = $conn->prepare("INSERT INTO persona (`cedula`, `nombre`, `apellido`) VALUES (?, ?, ?)");
+        $stmt->bind_param("iss", $cedula, $nombre, $apellido);
+
+        
+        $hashed_password = password_hash($contraseña, PASSWORD_BCRYPT);
+        $stmt1 = $conn->prepare("INSERT INTO usuario (`cedula`, `email`, `contraseña`) VALUES (?, ?, ?)");
+        $stmt1->bind_param("iss", $cedula, $email, $hashed_password);
+
+        
+        if ($stmt->execute() && $stmt1->execute()) {
+            $mensaje = "Registro exitoso";
+            
+            header('Location: ../html/lubricante.html');
+            exit(); 
         } else {
-            
-            $stmt = $conn->prepare("INSERT INTO persona (`cedula`, `nombre`, `apellido`) VALUES (?, ?, ?)");
-            $stmt->bind_param("iss", $cedula, $nombre, $apellido);
-            $stmt1 = $conn->prepare("INSERT INTO usuario (`cedula`, `email`, `contraseña`) VALUES (?, ?, ?)");
-            
-            
-            $hashed_password = password_hash($contraseña, PASSWORD_BCRYPT);
-            $stmt1->bind_param("iss", $cedula, $email, $contraseña);
-
-            if ($stmt->execute() && $stmt1->execute()) {
-                $mensaje = "Registro exitoso";
-                header('Location: ../html/lubricante.html');
-                
-            } else {
-                $error = true;
-                $mensaje = "Error al ejecutar la consulta: " . $stmt->error . " " . $stmt1->error;
-            }
-
-            $stmt->close();
-            $stmt1->close();
-            $conn->close();
+            $error = true;
+            $mensaje = "Error al ejecutar la consulta: " . $stmt->error . " " . $stmt1->error;
         }
+
+        
+        $stmt->close();
+        $stmt1->close();
+        $conn->close();
     }
+}
+
+echo $mensaje;
 
 ?>
