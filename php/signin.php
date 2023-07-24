b7602e158f258bb14fe914666facc55f5732d521
Michael574a <139634342+Michael574a@users.noreply.github.com>
2023-07-23 20:05:44 -0500

    signin contraseña
    signin contraseña

--- php/signin.php
+++ php/signin.php
@@ -30,7 +30,7 @@ if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         
         $hashed_password = password_hash($contraseña, PASSWORD_BCRYPT);
         $stmt1 = $conn->prepare("INSERT INTO usuario (`cedula`, `email`, `contraseña`) VALUES (?, ?, ?)");
-        $stmt1->bind_param("iss", $cedula, $email, $hashed_password);
+        $stmt1->bind_param("iss", $cedula, $email, $contraseña);
 
         
         if ($stmt->execute() && $stmt1->execute()) {
